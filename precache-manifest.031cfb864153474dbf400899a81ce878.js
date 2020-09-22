2290d256a60ece6ec4dcf4c7c45804f39e19f9e4
JunMinLi841221 <jli58@mail.sfsu.edu>
2020-09-21 21:25:16 -0700

    Updates

---
+++ precache-manifest.031cfb864153474dbf400899a81ce878.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9ea1c9a4af09c8d1b7f26f8a5e2f8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76be3c692776a668d3b",
+    "url": "/static/css/main.220c0139.chunk.css"
+  },
+  {
+    "revision": "5536ae86322a459240ff",
+    "url": "/static/js/2.8dffe265.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.8dffe265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76be3c692776a668d3b",
+    "url": "/static/js/main.ce425942.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e260d478f8338b89cb308aa0ca98551c",
+    "url": "/static/media/ACCESS.e260d478.png"
+  },
+  {
+    "revision": "97932ef343cf78174b215a11fb3ea347",
+    "url": "/static/media/After_show.97932ef3.JPG"
+  },
+  {
+    "revision": "bccb6ed113f1151c22b15b9c17871f36",
+    "url": "/static/media/Cat.bccb6ed1.JPG"
+  },
+  {
+    "revision": "e13ed6c7fb2019e86d04a50c5a851d8a",
+    "url": "/static/media/Dance.e13ed6c7.JPG"
+  },
+  {
+    "revision": "4f4fe0cf8fab8772a43fca4a6a84eecb",
+    "url": "/static/media/GD.4f4fe0cf.png"
+  },
+  {
+    "revision": "824bdb42488b4e7a8ccc3a823356117c",
+    "url": "/static/media/Kiss.824bdb42.JPG"
+  },
+  {
+    "revision": "2222a2e6c6c2de91b2ae964a983f13dc",
+    "url": "/static/media/Landscape.2222a2e6.JPG"
+  },
+  {
+    "revision": "e313dcd17557c374e080c6bb4875678f",
+    "url": "/static/media/Me.e313dcd1.png"
+  },
+  {
+    "revision": "4f16d430da3e68910e749a7b3ac5c397",
+    "url": "/static/media/Momentum.4f16d430.png"
+  },
+  {
+    "revision": "869b93a9631b94f5607e5f43a9a74a78",
+    "url": "/static/media/Real_modle.869b93a9.JPG"
+  },
+  {
+    "revision": "ddca869b46f5f0d75a8a2c3007bbf64a",
+    "url": "/static/media/Yawning.ddca869b.JPG"
+  }
+]);
